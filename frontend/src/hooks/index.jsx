78ed2f2b45208d2932764b359fb93582a59a4534
Artem Egorov <artegtor@gmail.com>
2023-03-23 13:07:52 +0400

    update hooks

--- frontend/src/hooks/index.jsx
+++ frontend/src/hooks/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import AuthContext from '../contexts/index.jsx'
+import AuthContext from '../contexts/index.jsx';
 
 const useAuth = () => useContext(AuthContext);
 
