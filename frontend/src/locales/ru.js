086bb927579e421fa7d95d443f051f330895bf9d
Artem Egorov <artegtor@gmail.com>
2023-03-23 13:11:17 +0400

    add frontend/src/locales

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,10 @@
+export default {
+  translation: {
+    messages: {
+      key_zero: '{{count}} сообщений',
+      key_one: '{{count}} сообщение',
+      key_few: '{{count}} сообщения',
+      key_many: '{{count}} сообщений',
+    },
+  },
+};
