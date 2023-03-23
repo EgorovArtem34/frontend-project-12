bef814cd8decb27a368112313e688e4651af58e5
Artem Egorov <artegtor@gmail.com>
2023-03-23 13:08:45 +0400

    eslint fix NotFoundPage.jsx

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,17 +1,15 @@
 import logo from '../assets/notFound.svg';
 
-const NotFoundPage = () => {
-  return (
-    <div className="text-center">
-      <img src={logo} className="img-fluid h-25" alt="Страница не найдена" />
-      <h1 className="h4 text-muted">Страница не найдена</h1>
-      <p className="text-muted">
-        Но вы можете перейти
-        {' '}
-        <a href="/">на главную страницу</a>
-      </p>
-    </div>
-  )
-};
+const NotFoundPage = () => (
+  <div className="text-center">
+    <img src={logo} className="img-fluid h-25" alt="Страница не найдена" />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      {' '}
+      <a href="/">на главную страницу</a>
+    </p>
+  </div>
+);
 
 export default NotFoundPage;
