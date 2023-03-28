6d4596f75e75d244f9d53a82ed9ca2de91d54682
Artem Egorov <artegtor@gmail.com>
2023-03-28 16:06:56 +0400

    add frontend/src/components/socketConfigure.jsx

---
+++ frontend/src/components/socketConfigure.jsx
@@ -0,0 +1,39 @@
+import store from '../slices/index.js';
+import { addMessage } from '../slices/messagesSlice.js';
+import { actions } from '../slices/channelsSlice.js';
+
+const socketConfigure = (socket) => {
+  const addNewMessage = (message) => {
+    socket.emit('newMessage', message, (response) => {
+      console.log(response.status);
+    });
+  };
+  const addNewChannel = (chanel) => {
+    socket.emit('newChannel', chanel);
+  };
+  const removeChannel = (chanelId) => {
+    socket.emit('removeChannel', chanelId);
+  };
+  const renameChannel = (chanel) => {
+    socket.emit('renameChannel', chanel);
+  };
+
+  socket.on('newMessage', (message) => {
+    store.dispatch(addMessage(message));
+  });
+  socket.on('newChannel', (channel) => {
+    store.dispatch(actions.addChannel(channel));
+    store.dispatch(actions.setCurrentChannel(channel.id));
+  });
+  socket.on('removeChannel', (chanelId) => {
+    store.dispatch(actions.removeChannel(chanelId));
+  });
+  socket.on('renameChannel', (channel) => {
+    store.dispatch(actions.renameChannel(channel));
+  });
+
+  return {
+    addNewMessage, addNewChannel, removeChannel, renameChannel,
+  };
+};
+export default socketConfigure;
