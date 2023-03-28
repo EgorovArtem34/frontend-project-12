93463032221f869c1ce475ffb371c1578d089022
Artem Egorov <artegtor@gmail.com>
2023-03-28 16:06:23 +0400

    frontend/src/components/ChatComponents/ Channels Messages

--- frontend/src/components/ChatComponents/Channels.jsx
+++ frontend/src/components/ChatComponents/Channels.jsx
@@ -1,44 +1,77 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, ButtonGroup } from 'react-bootstrap';
 import { BsPlusSquare } from 'react-icons/bs';
 import { actions } from '../../slices/channelsSlice.js';
+import { showModal } from '../../slices/modalsSlice.js';
 
 const Channels = () => {
+  const dispatch = useDispatch();
   const channelsData = useSelector(({ channelsSlice }) => channelsSlice.channelsData);
   const { channels, currentChannelId } = channelsData;
-  const dispatch = useDispatch();
+
+  const setShowModal = (type, item = null) => dispatch(showModal({ type, item }));
   const handleClick = (id) => {
     dispatch(actions.setCurrentChannel(id));
   };
+  const createStaticBtn = (channel) => (
+    <Button
+      onClick={() => handleClick(channel.id)}
+      type="button"
+      variant={channel.id === currentChannelId ? 'secondary' : ''}
+      className="w-100 rounded-0 text-start"
+    >
+      <span className="me-1">#</span>
+      {channel.name}
+    </Button>
+  );
+  const createChangeableBtn = (channel) => (
+    <Dropdown as={ButtonGroup} className="d-flex">
+      <Button
+        onClick={() => handleClick(channel.id)}
+        variant={channel.id === currentChannelId ? 'secondary' : ''}
+        className="w-100 rounded-0 text-start text-truncate btn"
+      >
+        <span className="me-1">#</span>
+        {channel.name}
+      </Button>
+
+      <Dropdown.Toggle
+        split
+        variant={channel.id === currentChannelId ? 'secondary' : 'light'}
+        id="dropdown-split-basic"
+      />
+
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => setShowModal('removeChannel', channel.id)}>
+          Удалить
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => setShowModal('renameChannel', channel.id)}>
+          Переименовать
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 
   return (
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>Каналы</b>
-        <Button className="p-0 text-primary btn-group-vertical" variant="outline-light">
+        <Button onClick={() => setShowModal('addChannel')} className="p-0 text-primary btn-group-vertical" variant="outline-light">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <BsPlusSquare />
           </svg>
         </Button>
       </div>
-      <div
+      <ul
         className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
         id="channels-box"
       >
         {channels.map((channel) => (
           <li className="nav-item w-100" key={channel.id}>
-            <Button
-              onClick={() => handleClick(channel.id)}
-              type="button"
-              variant={channel.id === currentChannelId ? 'secondary' : 'light'}
-              className="w-100 rounded-0 text-start"
-            >
-              <span className="me-1">#</span>
-              {channel.name}
-            </Button>
+            {channel.changeable ? createChangeableBtn(channel) : createStaticBtn(channel)}
           </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
--- frontend/src/components/ChatComponents/Messages.jsx
+++ frontend/src/components/ChatComponents/Messages.jsx
@@ -3,12 +3,19 @@ import { Button, Form, InputGroup } from 'react-bootstrap';
 import { useEffect, useRef } from 'react';
 import { BsArrowRightSquare } from 'react-icons/bs';
 import { useTranslation } from 'react-i18next';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import hooks from '../../hooks/index.jsx';
 
 const Messages = () => {
+  const { useAuth, useSocket } = hooks;
+  const socket = useSocket();
+  const { currentUser: { username } } = useAuth();
   const channelsData = useSelector(({ channelsSlice }) => channelsSlice.channelsData);
   const { channels, currentChannelId } = channelsData;
   const [currentChannel] = channels.filter((channel) => channel.id === currentChannelId);
   const messages = useSelector(({ messagesSlice }) => messagesSlice.messages);
+  const currentMessages = messages.filter((message) => message.channelId === currentChannelId);
   const { t } = useTranslation();
   const inputEl = useRef(null);
 
@@ -17,10 +24,30 @@ const Messages = () => {
   }, []);
 
   const createMessage = (message) => (
-    message
-    // <div class="text-break mb-2"><b>admin</b>: check</div>
+    <div className="text-break mb-2">
+      <b>{message.username}</b>
+      :
+      {' '}
+      {message.body}
+    </div>
   );
 
+  const signUpSchema = yup.object().shape({
+    body: yup.string()
+      .trim()
+      .required('Обязательное поле'),
+  });
+  const formik = useFormik({
+    initialValues: {
+      body: '',
+    },
+    validationSchema: signUpSchema,
+    onSubmit: (e, { resetForm }) => {
+      const { body } = e;
+      socket.addNewMessage({ body, channelId: currentChannelId, username });
+      resetForm();
+    },
+  });
   return (
     <div className="col p-0 h-100">
       <div className="d-flex flex-column h-100">
@@ -33,29 +60,31 @@ const Messages = () => {
             </b>
           </p>
           <span className="text-muted">
-            {t('messages.key', { count: messages.length })}
+            {t('messages.key', { count: currentMessages.length })}
           </span>
         </div>
         <div id="messages-box" className="chat-messages overflow-auto px-5">
-          {messages.map((message) => (
-            <div className="text-break mb-2" key={message}>
+          {currentMessages.map((message) => (
+            <div className="text-break mb-2" key={message.id}>
               {createMessage(message)}
             </div>
 
           ))}
         </div>
         <div className="mt-auto px-5 py-3">
-          <Form noValidate className="py-1 border rounded-2">
+          <Form onSubmit={formik.handleSubmit} className="py-1 border rounded-2">
             <InputGroup className="has-validation">
               <Form.Control
                 name="body"
+                onChange={formik.handleChange}
+                value={formik.values.body}
                 placeholder="Введите сообщение..."
                 aria-label="Новое сообщение"
                 aria-describedby="basic-addon2"
                 className="border-0 p-0 ps-2"
                 ref={inputEl}
               />
-              <Button type="submit" variant="group-vertical">
+              <Button type="submit" variant="group-vertical" disabled={formik.isSubmitting}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                   <BsArrowRightSquare />
                 </svg>
