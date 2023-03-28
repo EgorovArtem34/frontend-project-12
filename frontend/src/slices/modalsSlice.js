cc4c2d039114e1581ac7657a7215f7633b238ddc
Artem Egorov <artegtor@gmail.com>
2023-03-28 16:02:57 +0400

    add frontend/src/slices/modalsSlice.js

---
+++ frontend/src/slices/modalsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  type: null,
+  id: null,
+};
+
+const modalsSlice = createSlice({
+  name: 'modals',
+  initialState,
+  reducers: {
+    showModal: (state, { payload }) => {
+      state.type = payload.type;
+      state.id = payload.item;
+    },
+    closeModal: (state) => {
+      state.type = null;
+      state.id = null;
+    },
+  },
+});
+
+export const { showModal, closeModal } = modalsSlice.actions;
+
+export default modalsSlice.reducer;
